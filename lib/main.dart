93a24cfb66a9f2568866146745b63741797f0b79
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-02-21 21:43:58 -0500

    Add: File System Structure and new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,9 +14,7 @@ class MyApp extends StatelessWidget {
     
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text("Hello World")),
-      ) 
+      home: CounterScreen()
     );
   }
 }
