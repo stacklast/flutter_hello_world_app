7d70e3dfb7e64cca38310212ad26cb95021c68ba
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-02-21 21:24:49 -0500

    feat: First Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: Center(child: Text("Hello World"))
     );
   }
 }
