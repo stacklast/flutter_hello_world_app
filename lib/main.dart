63319e62cc00afa590e47ff8478b78df05855054
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-02-22 19:20:08 -0500

    feat: Add Material Design 3 theme and define color

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen()
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+      ),
+      home: const CounterScreen()
     );
   }
 }
