55189ba8ea6b4c5ca3d062108c4032e77673115c
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-02-22 19:36:32 -0500

    feat: Change State of the App

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: const Text('Counter Screen')),
+        title: const Text('Counter Screen'),
+        centerTitle: true,
       ),
-        body: const Center(
+      body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('10', style: TextStyle( fontSize: 160, fontWeight: FontWeight.w100),),
-              Text('Clicks', style: TextStyle( fontSize: 25))
-            ],
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-
-          },
-          child: const Icon(Icons.plus_one),
-        ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$clickCounter',
+            style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+          ),
+          const Text('Clicks', style: TextStyle(fontSize: 25))
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
+        child: const Icon(Icons.plus_one),
+      ),
     );
   }
 }
